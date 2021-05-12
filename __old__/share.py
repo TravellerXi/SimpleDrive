96692edee2e3cfe362494d69749b2a446b9c33ad
西 蔚 <admin@mytlu.cn>
2021-05-12 18:16:57 +0800

    更新路由

---
+++ __old__/share.py
@@ -0,0 +1,324 @@
+#!/usr/bin/python
+
+# coding:utf-8
+import pymysql
+import os
+#path = os.getcwd() + '/' + username + '/' + folders
+import random
+import string
+import time
+import datetime
+from __old__.sharehtml import *
+from __old__.thisdef import *
+from __old__.search_share_html import *
+
+sqlservername='localhost'
+sqluser='simpledrivedemo'
+sqlpasswd='simpledrivedemo'
+sqldatabase='simpledrivedemo'
+
+
+def checksharebyshareid(shareid):
+    db = pymysql.connect(sqlservername, sqluser, sqlpasswd, sqldatabase)
+    check = db.cursor()
+    sql = 'select relativepostion,fileorfoldername,path from '+sqldatabase+'.share where shareid=' + "'" + shareid + "'"
+    check.execute(sql)
+    if (check.fetchone()) is None:
+        db.close()
+        return 0
+    else:
+        ##该用户没有share
+        check.execute(sql)
+        result = check.fetchall()
+        resulttep=list(result)
+        resulttep=str(resulttep).replace('(','').replace(')','').replace('[','').replace(']','')
+        resulttep=list(resulttep.split(','))
+        if resulttep[2][0:1]==' ':
+            resulttep[2]=resulttep[2][1:]
+        resulttep[2]=resulttep[2].replace("'",'')
+        print('resulttep[2] is',resulttep[2])
+
+        if checkfileorfolder(resulttep[2])==1:
+            return 1
+        else:
+            return result
+
+
+def checkfileorfolderalreadyexistinshare(path):
+    db=pymysql.connect(sqlservername, sqluser, sqlpasswd, sqldatabase)
+    check=db.cursor()
+    sql='select * from '+sqldatabase+'.share where path=' + "'" + path + "'"
+    check.execute(sql)
+    if (check.fetchone()) is None:
+
+        db.close()
+
+        return 0
+
+    else:
+        return 1
+
+def CheckShareidSql(shareidsql):
+
+    db = pymysql.connect(sqlservername, sqluser, sqlpasswd, sqldatabase)
+
+    check = db.cursor()
+
+    SQL = 'select shareid from '+sqldatabase+'.share where shareid=' + "'" + shareidsql + "'"
+
+    check.execute(SQL)
+
+    if (check.fetchone()) is None:
+
+        db.close()
+
+        return 0
+
+    else:
+
+        check.execute(SQL)
+
+        result=tuple(check.fetchone())
+
+        result = ''.join(result)
+
+        if shareidsql == result:
+
+            db.close()
+
+            return 2
+
+        else:
+
+            db.close()
+
+            return 0
+
+def generateshareid():
+    i=1;r=''
+    s = string.ascii_lowercase
+    number=random.randint(999, 9999999)
+    while i<10:
+        r=r+random.choice (s)
+        i=i+1
+    randomnumber=r+str(number)
+    return randomnumber
+
+
+def checkfileorfolder(path):
+    if os.path.isdir(path):
+        return 1#folder
+    else:
+        return 2#file
+
+def sharefileorfolder(path,username,filename):
+    #print(path)
+    #print(path+filename)
+    if checkfileorfolderalreadyexistinshare(path+filename)==0:
+        db = pymysql.connect(sqlservername, sqluser, sqlpasswd, sqldatabase)
+        check = db.cursor()
+        if checkfileorfolder(path+filename) == 1:
+            username = username
+            path1 = path+filename
+            print(path1)
+            c='''\\'''
+            path1=str(path1).replace(c,'/')
+            relativepostion = str(path).replace(c,'/')
+            isdir = 'yes'
+            validto = sevendaysshijianchuo()
+            downloadedtime = '0'
+            entercode = ''
+            shareid = ''
+            fileorfoldername=filename
+            while 1:
+                shareid = generateshareid()
+                if CheckShareidSql(shareid) == 0:
+                    break
+                else:
+                    print('还没结束1')
+            print('#1',path1)
+            sqlpha = "insert into "+sqldatabase+".share (username,path,shareid,validto,downloadedtime,entercode,isdir,relativepostion,fileorfoldername) VALUES (" + "'" + username + "'" + "," + "'" + path1 + "'" + "," + "'" + shareid + "'" + "," + "'" + validto + "'" + "," + "'" + downloadedtime + "'" + "," + "'" + entercode + "'" + "," + "'" + isdir + "'""," + "'" + relativepostion + "'""," + "'" + fileorfoldername + "'" + ")"
+            # print(sqlpha)
+            check.execute(sqlpha)
+            db.commit()
+            check.close()
+        else:
+            username = username
+            path1 = path+filename
+            c = '''\\'''
+            path1 = str(path1).replace(c, '/')
+            isdir = 'no'
+            validto = sevendaysshijianchuo()
+            downloadedtime = '0'
+            entercode = ''
+            shareid = ''
+            relativepostion = str(path).replace(c,'/')
+            fileorfoldername=filename
+            while 1:
+                shareid = generateshareid()
+                if CheckShareidSql(shareid) == 0:
+                    break
+                else:
+                    print('还没结束2')
+            print('#2',path1)
+            #print('relativepostion is: ',relativepostion)
+            sqlpha = "insert into "+sqldatabase+".share (username,path,shareid,validto,downloadedtime,entercode,isdir,relativepostion,fileorfoldername) VALUES (" + "'" + username + "'" + "," + "'" + path1 + "'" + "," + "'" + shareid + "'" + "," + "'" + validto + "'" + "," + "'" + downloadedtime + "'" + "," + "'" + entercode + "'" + "," + "'" + isdir + "'""," + "'" + relativepostion + "'""," + "'" + fileorfoldername + "'" + ")"
+            # print(sqlpha)
+            check.execute(sqlpha)
+            db.commit()
+            check.close()
+        return 1
+    else:
+        return 0
+
+def sevendaysshijianchuo():
+    now = datetime.datetime.now()
+    dangqianshijian = datetime.datetime.strptime(now.strftime('%Y-%m-%d %H:%M:%S'), '%Y-%m-%d %H:%M:%S')
+    delta = datetime.timedelta(days=7)
+    sevendaylatertime = dangqianshijian + delta
+    sevendaylatertime_shijianchuo = time.mktime(sevendaylatertime.timetuple())
+    return  str(sevendaylatertime_shijianchuo)
+
+
+def checksharedbyusername(username):
+    db = pymysql.connect(sqlservername, sqluser, sqlpasswd, sqldatabase)
+    check=db.cursor()
+    sql='select path,shareid,validto,downloadedtime,entercode,isdir,relativepostion,fileorfoldername from '+sqldatabase+'.share where username=' + "'" + username + "'"
+    check.execute(sql)
+    n=0
+    if (check.fetchone()) is None:
+        db.close()
+        return 0
+    ##该用户没有share
+    else:
+        check.execute(sql)
+        result=check.fetchall()
+        #print(result)
+        return result
+
+def ReturnContentForCloud_ForcurrentFolder_share(username):
+    result=checksharedbyusername(username)
+    content = cloudhead_shared+username+preusername_shared
+    if Checkisadmin(username)>0:
+        content=content+adminpage_shared+afteradminpage_shared
+    else:
+        content=content+afteradminpage_shared
+    tmp=''
+    if checksharedbyusername(username)==0:
+        content=content+'当前用户没有分享'
+    else:
+        for i in result:
+            realpathwithfilename=i[0]
+            sharedid=i[1]
+            validto=i[2]
+            downloadedtime=i[3]
+            entercode=i[4]
+            isdir=i[5]
+            relativepostion=i[6]
+            #print(relativepostion)
+            fileorfoldername=i[7]
+            sharelink=sharedid
+            dirtype=''
+            currentposition='SimpleDrive/cloud/'+username
+            lenofusername=len(username)
+            thisislocation=relativepostion.find(currentposition)
+            #print(thisislocation)
+            xiangduiposition=relativepostion[thisislocation+18+lenofusername:]
+            #xiangduiposition=xiangduiposition[xiangduiposition.find('/'):]
+            #print(xiangduiposition)
+            if int(float(validto))==0:
+                validto='永久有效'
+            else:
+                if time.time()>int(float(validto)):
+                    validto='分享已过期'
+                else:
+                    dateArray = datetime.datetime.fromtimestamp(int(float(validto)))
+                    validto = dateArray.strftime("%Y--%m--%d %H:%M:%S")
+            if entercode=='':
+                entercode='无密码'
+            if isdir=='yes':
+                dirtype='文件夹'
+            else:
+                dirtype='文件'
+            tmp =tmp+ filedownloadurla_shared + sharedid + filedownloadurlb_shared + '/share/'+sharelink
+            tmp = tmp + beforefilename_shared +fileorfoldername + afterfilename_shared+dirtype+afterposition_shared+xiangduiposition+position
+            tmp = tmp + getfolderorfilesize(realpathwithfilename, '') + aftersize_shared + validto + afterfiletime_shared
+    content = content + (tmp)+cloudreset_shared
+    return content
+
+def alwaysvalid(shareid):
+    db = pymysql.connect(sqlservername, sqluser, sqlpasswd, sqldatabase)
+    check = db.cursor()
+    sql =  'update '+sqldatabase+'.share set validto =0 where shareid=' + "'" + shareid + "'"
+    try:
+        check.execute(sql)
+        db.commit()
+        db.close()
+        return 1
+    except:
+        return 0
+
+
+
+def cancelshare(shareid):
+    db = pymysql.connect(sqlservername, sqluser, sqlpasswd, sqldatabase)
+    check = db.cursor()
+    sql = 'delete  from '+sqldatabase+'.share where shareid=' + "'" + shareid + "'"
+    try:
+        check.execute(sql)
+        db.commit()
+        db.close()
+        return 1
+    except:
+        return 0
+
+def searchfromshared(username,searchcontent):
+    db = pymysql.connect(sqlservername, sqluser, sqlpasswd, sqldatabase)
+    check = db.cursor()
+    sql = 'select relativepostion,fileorfoldername from '+sqldatabase+'.share where username=' + "'" + username + "'"
+    check.execute(sql)
+    listforpathandfilename=[]
+    efolder = []
+    ffilename = []
+    if (check.fetchone()) is None:
+        db.close()
+        return 0
+    else:
+        ##该用户没有share
+        check.execute(sql)
+        result = check.fetchall()
+        #print(result)
+        for i in result:
+            if i[1].find(searchcontent)!=-1:
+                efolder.append(i[0] [:-1])
+                print(efolder)
+                ffilename.append(i[1])
+    #print(efolder)
+    return (returnsearchhtmlforshare(efolder,ffilename,username))
+
+
+def returnsearchhtmlforshare(path,filename,username):
+    #print(path)
+    #print(type(path))
+    content = cloudhead_search + username + preusername_search
+    ###try to close the door for non-admin user.
+    if Checkisadmin(username) > 0:
+        content = content + adminpage_search + afteradminpage_search
+    else:
+        content = content + afteradminpage_search
+    count=0
+    tempath=os.getcwd()+'/'+username
+    for i in path:
+        i=i.replace('\\','/')
+        #print(i)
+        tmp = filedownloadurla_search + '' + filedownloadurlb_search  + '/folder'+i[len(tempath)+len(username)+1:]
+        #print('alert:')
+        #print(i[len(tempath)+len(username)+1:])
+        tmp = tmp + beforefilename_search  + filename[count]  + afterfilename_search
+        tmp = tmp + getfolderorfilesize(i+'/',filename[count]) + aftersize_search + getfilecreatetime(i+'/',filename[count]) + afterfiletime_search
+        content = content + (tmp)
+        count=count+1
+
+    content = content + cloudreset_search
+    return content
+
--- route.py
+++ route.py
@@ -6,6 +6,7 @@ from flask import request
 from flask import redirect
 from flask import send_from_directory
 import os
+import shutil
 app =Flask(__name__)
 
 @app.route('/login', methods=['GET'])
@@ -107,3 +108,173 @@ def lib_get(filename):
             return send_from_directory(path, path=thisisfilename, as_attachment=True)
         else:
             return send_from_directory(path1, path=thisisfilename, as_attachment=True)
+
+@app.route('/file/<path:filename>',methods=['GET'])
+def file_get(filename):
+    username = request.cookies.get('username')
+    md5credit = request.cookies.get('credit')
+    from modules.Sql import SqlMethod
+    Sql=SqlMethod()
+    if Sql.Checkmd5(md5credit, username) == 0:
+        return redirect('/login')
+    else:
+        path = os.getcwd() + '/cloud/' + username
+        from __old__.thisdef import checkFilenameAndFolder
+        nextpath = checkFilenameAndFolder(filename)[1]
+        path1 = path + '/' + nextpath
+
+        thisisfilename = checkFilenameAndFolder(filename)[0]
+        if thisisfilename[0:1] == ' ':
+            if nextpath == 'nodirhere':
+                return send_from_directory(path, path=thisisfilename[1:], as_attachment=True)
+            else:
+                return send_from_directory(path1, path=thisisfilename[1:], as_attachment=True)
+        else:
+            if nextpath == 'nodirhere':
+                return send_from_directory(path, path=thisisfilename, as_attachment=True)
+            else:
+                return send_from_directory(path1, path=thisisfilename, as_attachment=True)
+
+@app.route('/folder/pan/<path:filename>',methods=['GET'])
+def folder_pan_get(filename):
+    username = request.cookies.get('username')
+    md5credit = request.cookies.get('credit')
+    from modules.Sql import SqlMethod
+    Sql=SqlMethod()
+    if Sql.Checkmd5(md5credit, username) == 0:
+        return redirect('/login')
+    else:
+        return redirect('/pan/' + filename)
+
+@app.route('/folder/<path:folders>', methods=['POST'])
+def folderpath_POST(folders):
+    from modules.folder import ReturnContentForCloud_ForcurrentFolder, ReturnContentForCloud_ForcurrentFolder
+    username = request.cookies.get('username')
+    md5credit = request.cookies.get('credit')
+    from modules.Sql import SqlMethod
+    Sql=SqlMethod()
+    if Sql.Checkmd5(md5credit, username) == 0:
+        return redirect('/login')
+    else:
+        path = os.getcwd() + '/cloud/' + username + '/' + folders
+        pathtemp = path + '/'
+        if request.form['formname'] == 'fileorfolderlist':
+            mycheck = request.form.getlist('mycheck')
+            if request.form['submit'] == '删除':
+                for i in mycheck:
+                    if i[0:1] == ' ':
+                        i = i[1:]
+                    try:
+                        os.remove(pathtemp + i)
+                    except:
+                        import shutil
+                        shutil.rmtree(pathtemp + i)
+                from __old__.thisdef import ReturnContentForCloud_ForcurrentFolder
+                return ('删除完毕<br>' + ReturnContentForCloud_ForcurrentFolder(path, username, folders))
+
+            elif request.form['submit'] == '分享':
+                fenxiang = 1
+                for i in mycheck:
+                    if i[0:1] == ' ':
+                        i = i[1:]
+                    pathshare = pathtemp + i
+                    from __old__.share import sharefileorfolder
+                    fenxiang = sharefileorfolder(path + '/', username, i)
+                if fenxiang == 0:
+                    return ('该文件/文件夹已经分享过了<br>' + ReturnContentForCloud_ForcurrentFolder(path, username, folders))
+                else:
+                    return ('分享成功,请去已分享查询<br>' + ReturnContentForCloud_ForcurrentFolder(path, username, folders))
+        elif request.form['formname'] == 'search':
+            pathsearch = path
+            #print(pathsearch)
+            searchinfo=request.form['search']
+            from __old__.thisdef import searchfilename
+            b=searchfilename(searchinfo,pathsearch,username)
+            return (b)
+
+
+
+        elif request.form['formname'] == 'newfile':
+            path = os.getcwd() + '/cloud/' + username + '/' + folders
+            file = request.files.get('file')
+            pathwithfilename = path + '/' + file.filename
+            file.save(pathwithfilename)
+            return ('文件上传成功,页面应该刷新!<br>' + ReturnContentForCloud_ForcurrentFolder(path, username, folders))
+        elif request.form['formname'] == 'newfolder':
+            newfolder = str(request.form['newfoldername'])
+            pathnew = os.getcwd() + '/cloud/' + username + '/' + folders + '/' + newfolder
+            path = os.getcwd() + '/cloud/' + username + '/' + folders
+            # os.system('mkdir -p '+pathnew)
+            os.mkdir(pathnew, 777)
+            # print(path)
+            return ('文件夹创建成功,页面应该刷新!<br>' + ReturnContentForCloud_ForcurrentFolder(path, username, folders))
+        else:
+            return ('禁止调皮')
+
+@app.route('/folder',methods=['POST'])
+def folderroot_POST():
+    from __old__.thisdef import ReturnContentForCloud_ForcurrentFolder
+    username = request.cookies.get('username')
+    md5credit = request.cookies.get('credit')
+    from modules.Sql import SqlMethod
+    Sql=SqlMethod()
+    if Sql.Checkmd5(md5credit, username) == 0:
+        return redirect('/login')
+    else:
+        path = os.getcwd().replace('\\', '/') + '/cloud/' + username
+        pathtemp = path + '/'
+        if request.form['formname'] == 'fileorfolderlist':
+            mycheck = request.form.getlist('mycheck')
+            if request.form['submit'] == '删除':
+                for i in mycheck:
+                    if i[0:1] == ' ':
+                        i = i[1:]
+                    try:
+                        os.remove(pathtemp + i)
+                    except:
+                        shutil.rmtree(pathtemp + i)
+
+                from __old__.thisdef import ReturnContentForCloud_ForcurrentFolder
+                return ('删除完毕<br>' + ReturnContentForCloud_ForcurrentFolder(path, username, ''))
+
+            elif request.form['submit'] == '下载':
+                from __old__.thisdef import ReturnContentForCloud_ForcurrentFolder
+                return ('暂时不支持文件批量下载<br>' + ReturnContentForCloud_ForcurrentFolder(path, username, ''))
+
+            elif request.form['submit'] == '分享':
+                fenxiang = 1
+                for i in mycheck:
+                    if i[0:1] == ' ':
+                        i = i[1:]
+                    pathshare = pathtemp + i
+                    from __old__.share import sharefileorfolder
+                    fenxiang = sharefileorfolder(path + '/', username, i)
+                if fenxiang == 0:
+                    return ('该文件/文件夹已经分享过了<br>' + ReturnContentForCloud_ForcurrentFolder(path, username, ''))
+                else:
+                    return ('分享成功,请去已分享查询<br>' + ReturnContentForCloud_ForcurrentFolder(path, username, ''))
+        elif request.form['formname'] == 'search':
+            path = os.getcwd().replace('\\', '/') + '/cloud/' + username
+            searchinfo=request.form['search']
+            from __old__.thisdef import searchfilename
+            b=searchfilename(searchinfo,path,username)
+            return (b)
+
+
+
+        elif request.form['formname'] == 'newfile':
+            path = os.getcwd() + '/cloud/' + username + '/'
+            file = request.files.get('file')
+            pathwithfilename = path + '/' + file.filename
+            # path='D:\Config'
+            file.save(pathwithfilename)
+            return ('文件上传成功,页面应该刷新!<br>' + ReturnContentForCloud_ForcurrentFolder(path, username, ''))
+        elif request.form['formname'] == 'newfolder':
+            newfolder = str(request.form['newfoldername'])
+            pathnew = os.getcwd() + '/cloud/' + username + '/' + newfolder
+            path = os.getcwd() + '/cloud/' + username
+            # os.system('mkdir -p ' + pathnew)
+            os.mkdir(pathnew, 777)
+            # print(path)
+            return ('文件夹创建成功,页面应该刷新!<br>' + ReturnContentForCloud_ForcurrentFolder(path, username, ''))
+
